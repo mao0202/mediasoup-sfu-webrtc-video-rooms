c22bd48e64196162c9ad87a4c1d78eaf6f46b988
毛锦鑫 <maojinxin0202@qq.com>
2022-11-17 16:07:37 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        tabWidth: 4
+      }
+    }
+  ]
+}
---
+++ src/config.js
@@ -0,0 +1,104 @@
+const os = require('os')
+const ifaces = os.networkInterfaces()
+
+//get the  ip of  the local open
+const getLocalIp = () => {
+  let localIp = '127.0.0.1'
+  Object.keys(ifaces).forEach((ifname) => {
+    for (const iface of ifaces[ifname]) {
+      // Ignore IPv6 and 127.0.0.1
+      if (iface.family !== 'IPv4' || iface.internal !== false) {
+        continue
+      }
+      // Set the local ip to the first IPv4 address found and exit the loop
+      localIp = iface.address
+      return
+    }
+  })
+  //console.log(localIp);
+  return localIp
+}
+
+module.exports = {
+  listenIp: '0.0.0.0',
+  listenPort: 3016,
+  sslCrt: '../ssl/cert.pem',
+  sslKey: '../ssl/key.pem',
+
+  mediasoup: {
+    // Worker settings
+    numWorkers: Object.keys(os.cpus()).length,
+    worker: {
+      rtcMinPort: 10000,
+      rtcMaxPort: 10100,
+      logLevel: 'warn',
+      logTags: [
+        'info',
+        'ice',
+        'dtls',
+        'rtp',
+        'srtp',
+        'rtcp',
+         'rtx',
+        // 'bwe',
+        'score'
+       // 'simulcast',
+        //'svc'
+      ]
+    },
+    // Router settings
+    router: {
+      mediaCodecs: [
+        {
+          kind: 'audio',
+          mimeType: 'audio/opus',
+          clockRate: 48000,
+          channels: 2
+        },
+        {
+          kind: 'video',
+          mimeType: 'video/VP8',
+          clockRate: 90000,
+          parameters: {
+            'x-google-start-bitrate': 1000
+          }
+        },
+        {
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '4d0032',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
+					}
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '42e01f',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
+					}
+				}
+      ]
+    },
+    // WebRtcTransport settings
+    webRtcTransport: {
+      listenIps: [
+        {
+          ip: '0.0.0.0',
+          announcedIp:'192.168.0.144'// replace by public IP address
+        }
+      ],
+      maxIncomingBitrate: 15000000,
+      initialAvailableOutgoingBitrate: 10000000
+    }
+  }
+}
